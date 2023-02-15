4a53a78bc03fb35d3b19bffdce33e9b53481a14e
Shreya <pesug20cs410@pesu.pes.edu>
2023-02-15 23:43:07 +0530

    added new c++ file

---
+++ PES2UG20CS410_task5.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "I have successfully built and run\n";
+  cout<<"I am executing task 5 of cc assignment. \n";
+  return 0;
+}
