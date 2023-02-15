702e49726cd9145fbb69159c4dca0c72aefc00e8
Shreya <pesug20cs410@pesu.pes.edu>
2023-02-15 23:28:08 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, PES2UG20CS410 Jenkins\n";
+  cout << "Hello, PES2UG20CS410 Shreya Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
